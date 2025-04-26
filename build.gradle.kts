14e357ee07c0f3fc6684997bc47718d281da28ea
Valentin Shabaev <vl7shab@yandex.ru>
2025-04-26 12:42:35 +0300

    add depens

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation(files("lib/antlr-4.13.2-complete.jar"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
